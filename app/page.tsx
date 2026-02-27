b107286d3246df08200cad892af86329d3aa4271
prajna <prajna085@gmail.com>
2026-02-27 09:39:26 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,418 @@
-import Image from "next/image";
+//-----------------------------------------Uncontrolled Component-------------------------------------------------
+// "use client";
+// import { useRef, FormEvent } from "react";
 
-export default function Home() {
+// export default function Home() {
+//   const inputRef = useRef<HTMLInputElement | null>(null);
+
+//   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+
+//     if (inputRef.current) {
+//       console.log(inputRef.current.value);
+//     }
+//   };
+
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+//         <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-96">
+//             <input
+//             type="text"
+//             ref={inputRef}
+//             className="w-full p-2 border rounded mb-4"
+//             placeholder="Enter name"
+//             />
+  
+//               <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">
+//                 Submit
+//                 </button>
+//         </form>
+//     </div>
+//     );
+// }
+
+
+//----------------------------------------Controlled component-----------------------------------------------------
+//api thing
+// "use client";
+
+// import { useState, ChangeEvent, FormEvent } from "react";
+
+// interface FormData {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
+
+// interface FormErrors {
+//   name?: string;
+//   email?: string;
+//   password?: string;
+// }
+
+// export default function LoginPage() {
+
+//   const [formData, setFormData] = useState<FormData>({
+//     name: "",
+//     email: "",
+//     password: ""
+//   });
+
+//   const [errors, setErrors] = useState<FormErrors>({});
+//   const [loading, setLoading] = useState<boolean>(false);
+//   const [apiError, setApiError] = useState<string | null>(null);
+//   const [success, setSuccess] = useState<boolean>(false);
+
+
+//   // handle input change
+//   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setFormData(prev => ({
+//       ...prev,
+//       [name]: value
+//     }));
+//   };
+
+
+//   // validate form
+//   const validate = (): FormErrors => {
+//     const newErrors: FormErrors = {};
+
+//     if (!formData.name.trim()) {
+//       newErrors.name = "Name is required";
+//     }
+
+//     if (!formData.email.trim()) {
+//       newErrors.email = "Email is required";
+//     } 
+//     else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+//       newErrors.email = "Invalid email";
+//     }
+
+//     if (!formData.password) {
+//       newErrors.password = "Password is required";
+//     } 
+//     else if (formData.password.length < 6) {
+//       newErrors.password = "Minimum 6 characters";
+//     }
+
+//     return newErrors;
+//   };
+
+
+//   // handle submit
+//   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+
+//     e.preventDefault();
+
+//     setSuccess(false);
+//     setApiError(null);
+
+//     const validationErrors = validate();
+
+//     if (Object.keys(validationErrors).length > 0) {
+//       setErrors(validationErrors);
+//       return;
+//     }
+
+//     setErrors({});
+
+//     try {
+
+//       setLoading(true);
+
+//       const response = await fetch(
+//         "https://jsonplaceholder.typicode.com/posts",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json"
+//           },
+//           body: JSON.stringify(formData)
+//         }
+//       );
+
+//       if (!response.ok) {
+//         throw new Error("Login failed");
+//       }
+
+//       const data = await response.json();
+
+//       console.log(data);
+
+//       setSuccess(true);
+
+//       // clear form
+//       setFormData({
+//         name: "",
+//         email: "",
+//         password: ""
+//       });
+
+//     } 
+//     catch (error) {
+
+//       if (error instanceof Error) {
+//         setApiError(error.message);
+//       }
+
+//     } 
+//     finally {
+//       setLoading(false);
+//     }
+
+//   };
+
+
+//   return (
+
+//     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+
+//       <form
+//         onSubmit={handleSubmit}
+//         className="bg-white p-6 rounded-lg shadow-md w-80"
+//       >
+
+//         <h2 className="text-xl font-bold mb-4 text-center">
+//           Login Form
+//         </h2>
+
+
+//         {/* Name */}
+//         <input
+//           type="text"
+//           name="name"
+//           placeholder="Name"
+//           value={formData.name}
+//           onChange={handleChange}
+//           className="w-full border p-2 mb-1 rounded"
+//         />
+
+//         {errors.name && (
+//           <p className="text-red-500 text-sm mb-2">
+//             {errors.name}
+//           </p>
+//         )}
+
+
+//         {/* Email */}
+//         <input
+//           type="email"
+//           name="email"
+//           placeholder="Email"
+//           value={formData.email}
+//           onChange={handleChange}
+//           className="w-full border p-2 mb-1 rounded"
+//         />
+
+//         {errors.email && (
+//           <p className="text-red-500 text-sm mb-2">
+//             {errors.email}
+//           </p>
+//         )}
+
+
+//         {/* Password */}
+//         <input
+//           type="password"
+//           name="password"
+//           placeholder="Password"
+//           value={formData.password}
+//           onChange={handleChange}
+//           className="w-full border p-2 mb-1 rounded"
+//         />
+
+//         {errors.password && (
+//           <p className="text-red-500 text-sm mb-2">
+//             {errors.password}
+//           </p>
+//         )}
+
+
+//         {/* Button */}
+//         <button
+//           type="submit"
+//           disabled={loading}
+//           className="w-full bg-blue-500 text-white p-2 rounded mt-2"
+//         >
+//           {loading ? "Loading..." : "Login"}
+//         </button>
+
+
+//         {/* API error */}
+//         {apiError && (
+//           <p className="text-red-500 mt-2 text-sm">
+//             {apiError}
+//           </p>
+//         )}
+
+
+//         {/* success */}
+//         {success && (
+//           <p className="text-green-600 mt-2 text-sm">
+//             Login successful!
+//           </p>
+//         )}
+
+//       </form>
+
+//     </div>
+
+//   );
+
+// }
+
+"use client";
+import { useEffect, useState } from "react";
+import { set } from "react-hook-form";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+  phone: string;
+}
+
+export default function UsersPage() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+
+        const data: User[] = await response.json();
+        setUsers(data);
+
+      } catch (err) {
+        if(err instanceof Error) {
+          setError(err.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return( <div className="min-h-screen flex items-center justify-center">
+      <p className="text-lg font-semibold">Loading users...</p>
+      </div>
+      );
+  }
+
+  if (error) {
+    return (
+    <div className="min-h-screen flex items-center justify-center">
+    <p className="text-red-500">{error}</p>
+    </div>
+    );
+  }
+
+  if (users.length === 0) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>No users found.</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h2 className="text-2xl font-bold mb-6 text-center">
+        Users List
+      </h2>
+      <div className="grid gap-4 max-w-3xl mx-auto">
+        {users.map(user => (
+          <div
+           key={user.id} 
+          className="bg-white p-4 rounded shadow">
+
+        <h3 className="text-lg font-semibold">{user.name}</h3>
+        <p className="text-sm text-gray-600">{user.email}</p>
+          <p className="text-sm text-gray-600">{user.username}</p>
+          <p className="text-sm text-gray-600">{user.phone}</p>
+          </div>
+        ))}
+    </div>
     </div>
   );
 }
+
+
+
+{/* //---------------------------------------------------------------------------------------------------------------
+// "use client";
+// import {useForm} from "react-hook-form";
+
+// interface LoginFormInputs {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
+
+// export default function Home() {
+//   const { 
+//     register,
+//     handleSubmit,
+//     formState: { errors ,isSubmitting },
+//     } = useForm<LoginFormInputs>();
+
+//     const onSubmit = (data: LoginFormInputs) => {
+//       console.log("Form Data:", data) ;
+//     };
+
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+//         <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-6 rounded shadow-md w-96">
+//           <h2 className="text-2xl font-bold mb-4">Login Form</h2>
+
+//           <input
+//             type="text"
+//             placeholder="Enter name"
+//             {...register("name", { required: "Name is required" })}
+//             className="w-full p-2 border rounded mb-4"
+//             />
+
+//             {errors.name && <p className="text-red-500 text-sm mb-2">{errors.name.message}</p>}
+
+//             <input
+//             type="email"
+//             placeholder="Enter email"
+//             {...register("email", { required: "Email is required", 
+//             pattern: { value: /\S+@\S+\.\S+/, message: "Email is invalid" } })}
+//             className="w-full p-2 border rounded mb-4"
+//             />
+
+//             {errors.email && <p className="text-red-500 text-sm mb-2">{errors.email.message}</p>}
+        
+//             <input
+//             type="password"
+//             placeholder="Enter password"
+//             {...register("password", { required: "Password is required", 
+//             minLength: { value: 6, message: "Password must be at least 6 characters" } })}
+//             className="w-full p-2 border rounded mb-4"
+//             />
+
+//             {errors.password && <p className="text-red-500 text-sm mb-2">{errors.password.message}</p>} 
+//               <button type="submit" disabled={isSubmitting} className="w-full bg-blue-500 text-white p-2 rounded">
+//                 {isSubmitting ? "Submitting..." : "Submit"}
+//                 </button>
+//         </form>
+//     </div>
+//     );
+// } */}
